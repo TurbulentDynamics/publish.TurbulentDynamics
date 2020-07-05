8e0b8a1c5d0aa2fab91fca03068367039186d98e
Niall Ó Broin <niall@turbulentdynamics.com>
2020-07-05 17:50:28 +0200

    Initial Commit

---
+++ Content/posts/first-post.md
@@ -0,0 +1,8 @@
+---
+date: 2020-06-19 21:27
+description: A description of my first post.
+tags: first, article
+---
+# My first post
+
+My first post's text.
---
+++ Sources/TurbulentDynamics/main.swift
@@ -0,0 +1,34 @@
+import Foundation
+import Publish
+import Plot
+
+// This type acts as the configuration for your website.
+struct NiallOBroin: Website {
+    enum SectionID: String, WebsiteSectionID {
+        // Add the sections that you want your website to contain here:
+        case Blog
+        case Consultancy
+        case MachineLearning
+    }
+
+    struct ItemMetadata: WebsiteItemMetadata {
+        // Add any site-specific metadata that you want to use here.
+    }
+
+    // Update these properties to configure your website:
+    var url = URL(string: "https://TurbulentDynamics.com")!
+    var title = "TurbulentDynamics.github.io"
+    var name = "Turbulent Dynamics"
+    var description = "Some Photos and Words from Turbulent Dynamics"
+    var language: Language { .english }
+    var imagePath: Path? { nil }
+//    var socialMediaLinks: [SocialMediaLink] { [.location, .email, .linkedIn, .github, .twitter] }
+}
+
+// This will generate your website using the built-in Foundation theme:
+try NiallOBroin().publish(withTheme: .foundation,
+                          deployedUsing: .gitHub("TurbulentDynamics/TurbulentDynamics.github.io.publish"))
+
+
+
+
